1a747ca4ee6934d8ca5a09efdd78eb623f56fec1
Haoxiang Li <hli18@host-nonstudentw-158-122.dhcp.stevens-tech.edu>
2012-08-13 20:41:53 -0400

    Initial Commit

---
+++ MotionPattern/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  MotionPattern
+//
+//  Created by Haoxiang Li on 8/13/12.
+//  Copyright (c) 2012 Haoxiang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
